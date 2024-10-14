88c0b95b0964920edf3738532a0f04d190dd4e5d
Marija <marija@Apples-MacBook-Pro.local>
2024-10-14 22:26:36 +0200

    remove highlight

--- src/layouts/ServicesCard.jsx
+++ src/layouts/ServicesCard.jsx
@@ -2,7 +2,7 @@ const ServicesCard = ({ icon, title, text, secondTitle, onClick }) => {
   return (
     <div
       onClick={onClick}
-      className="no-highlight group flex flex-col mt-6 lg:mt-10 items-center text-center gap-2 w-full lg:w-1/3 p-5 shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] rounded-lg cursor-pointer lg:hover:-translate-y-6 lg:transition duration-300 lg:ease-in-out"
+      className="no-highlight  group flex flex-col mt-6 lg:mt-10 items-center text-center gap-2 w-full lg:w-1/3 p-5 shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] rounded-lg cursor-pointer lg:hover:-translate-y-6 lg:transition duration-300 lg:ease-in-out"
     >
       <div className="bg-[#d5f2ec] p-3 rounded-full lg:transition-colors lg:duration-300 ease-in-out lg:group-hover:bg-[#ade9dc]">
         {icon}
